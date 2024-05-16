3022fa6dd40415fcfbebe548142d035af6b094fc
lucasfsf <lucasfreireri@gmail.com>
2024-05-16 12:13:01 +0000

    make  more robust to error handling

--- infra/database.js
+++ infra/database.js
@@ -10,9 +10,14 @@ async function query(queryObject) {
   });
   await client.connect();
 
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
