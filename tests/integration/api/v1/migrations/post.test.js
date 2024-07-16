d28877971004bffbde3ed79a910773be44336e97
lucasfsf <lucasfreireri@gmail.com>
2024-07-16 14:55:40 +0000

    makes `npm test` more robust with `async-retry` and `orchestrator.js`

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,10 +1,11 @@
 import database from "infra/database";
+import orchestrator from "tests/orchestrator";
 
-beforeAll(cleanDatabase);
-
-async function cleanDatabase() {
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  // clean db
   await database.query("drop schema public cascade; create schema public;");
-}
+});
 
 test("POST to /api/v1/migrations should return 201", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations", {
